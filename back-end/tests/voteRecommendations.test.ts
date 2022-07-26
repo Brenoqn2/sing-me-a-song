074da7e5b297b487372cdf7236f9c13f8e1b39fc
Brenoqn2 <brenoqn2@gmail.com>
2022-07-26 20:11:50 -0300

    added integrations tests to POST /recommendations/:id/upvote

---
+++ back-end/tests/voteRecommendations.test.ts
@@ -0,0 +1,67 @@
+import app from "../src/app";
+import supertest from "supertest";
+import { prisma } from "../src/database.js";
+import { generateNewRecommendationBody } from "./factories/recommendationsFactory.js";
+
+const agent = supertest(app);
+
+beforeEach(async () => {
+  await prisma.$executeRaw`TRUNCATE TABLE recommendations`;
+});
+
+describe("POST /recommendations/:id/upvote", () => {
+  it("given valid id, should return 200", async () => {
+    const body = generateNewRecommendationBody();
+    const recommendation = await prisma.recommendation.create({
+      data: {
+        name: body.name,
+        youtubeLink: body.youtubeLink,
+      },
+    });
+    const response = await agent.post(
+      `/recommendations/${recommendation.id}/upvote`
+    );
+    expect(response.status).toBe(200);
+  });
+
+  it("given invalid id, should return 404", async () => {
+    const response = await agent.post(`/recommendations/-1/upvote`);
+    expect(response.status).toBe(404);
+  });
+
+  it("given not numeric id, should return 404", async () => {
+    const response = await agent.post(`/recommendations/a/upvote`);
+    expect(response.status).toBe(404);
+  });
+
+  it("given valid id, body should be empty", async () => {
+    const body = generateNewRecommendationBody();
+    const recommendation = await prisma.recommendation.create({
+      data: {
+        name: body.name,
+        youtubeLink: body.youtubeLink,
+      },
+    });
+    const response = await agent.post(
+      `/recommendations/${recommendation.id}/upvote`
+    );
+    expect(response.body).toEqual({});
+  });
+
+  it("given valid id, should increase score by 1", async () => {
+    const body = generateNewRecommendationBody();
+    const recommendation = await prisma.recommendation.create({
+      data: {
+        name: body.name,
+        youtubeLink: body.youtubeLink,
+      },
+    });
+    await agent.post(`/recommendations/${recommendation.id}/upvote`);
+    const recommendationAfter = await prisma.recommendation.findUnique({
+      where: {
+        id: recommendation.id,
+      },
+    });
+    expect(recommendationAfter.score).toBe(1);
+  });
+});
