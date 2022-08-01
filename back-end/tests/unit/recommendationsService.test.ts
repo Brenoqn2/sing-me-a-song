9445536959a657331016602c9fb3fe9395451bc7
Brenoqn2 <brenoqn2@gmail.com>
2022-07-31 21:35:14 -0300

    added unit tests to insert, upvote and downvote functions of recommendationsService

---
+++ back-end/tests/unit/recommendationsService.test.ts
@@ -0,0 +1,97 @@
+import { recommendationService } from "../../src/services/recommendationsService.js";
+import { recommendationRepository } from "../../src/repositories/recommendationRepository.js";
+import { jest } from "@jest/globals";
+import { generateNewRecommendationBody } from "../factories/recommendationsFactory.js";
+
+describe("insert", () => {
+  it("should call recommendationRepository.create once", async () => {
+    jest
+      .spyOn(recommendationRepository, "create")
+      .mockImplementationOnce((): any => null);
+
+    await recommendationService.insert(generateNewRecommendationBody());
+    expect(recommendationRepository.create).toHaveBeenCalledTimes(1);
+  });
+
+  it("should throw an error if recommendation name already exists", async () => {
+    jest
+      .spyOn(recommendationRepository, "findByName")
+      .mockImplementationOnce((): any => generateNewRecommendationBody());
+
+    await expect(
+      recommendationService.insert(generateNewRecommendationBody())
+    ).rejects.toEqual({
+      message: "Recommendations names must be unique",
+      type: "conflict",
+    });
+  });
+});
+
+describe("upvote", () => {
+  it("should call recommendationRepository.updateScore once", async () => {
+    jest
+      .spyOn(recommendationRepository, "find")
+      .mockImplementationOnce((): any => generateNewRecommendationBody());
+    jest
+      .spyOn(recommendationRepository, "updateScore")
+      .mockImplementationOnce((): any => null);
+
+    await recommendationService.upvote(1);
+    expect(recommendationRepository.updateScore).toHaveBeenCalledTimes(1);
+  });
+
+  it("should throw an error if recommendation does not exist", async () => {
+    jest
+      .spyOn(recommendationRepository, "find")
+      .mockImplementationOnce((): any => null);
+
+    await expect(recommendationService.upvote(1)).rejects.toEqual({
+      message: "",
+      type: "not_found",
+    });
+  });
+});
+
+describe("downvote", () => {
+  it("should call recommendationRepository.updateScore", async () => {
+    jest
+      .spyOn(recommendationRepository, "find")
+      .mockImplementationOnce((): any => generateNewRecommendationBody());
+    jest
+      .spyOn(recommendationRepository, "updateScore")
+      .mockImplementationOnce((): any => {
+        return { score: 0 };
+      });
+
+    await recommendationService.downvote(1);
+    expect(recommendationRepository.updateScore).toHaveBeenCalled;
+  });
+
+  it("should throw an error if recommendation does not exist", async () => {
+    jest
+      .spyOn(recommendationRepository, "find")
+      .mockImplementationOnce((): any => null);
+
+    await expect(recommendationService.downvote(1)).rejects.toEqual({
+      message: "",
+      type: "not_found",
+    });
+  });
+
+  it("should remove recommendation if score is below -5", async () => {
+    jest
+      .spyOn(recommendationRepository, "find")
+      .mockImplementationOnce((): any => generateNewRecommendationBody());
+    jest
+      .spyOn(recommendationRepository, "updateScore")
+      .mockImplementationOnce((): any => {
+        return { score: -5 };
+      });
+    jest
+      .spyOn(recommendationRepository, "remove")
+      .mockImplementationOnce((): any => null);
+
+    await recommendationService.downvote(1);
+    expect(recommendationRepository.remove).toHaveBeenCalled;
+  });
+});
